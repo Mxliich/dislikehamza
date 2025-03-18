a52df22c5ff7d0b67bec9977884e1b0e447bc5f2
Mxliich <162749408+Mxliich@users.noreply.github.com>
2025-03-18 02:13:40 +0200

    Update counter.js

--- models/server/counter.js
+++ models/server/counter.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const counterSchema = new mongoose.Schema({
-    name: String,
-    count: Number,
+    name: { type: String, required: true, unique: true },
+    count: { type: Number, default: 0 }
 });
 
-module.exports = mongoose.model('Counter', counterSchema);
+module.exports = mongoose.model('Counter', counterSchema); 
